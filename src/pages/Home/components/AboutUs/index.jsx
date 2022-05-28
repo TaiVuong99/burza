25bedd2871af80e77c478f19bae05bcafdaf758d
Tài Vương <taivuongduy2@gmail.com>
2022-05-29 00:12:37 +0700

    ui fix

---
+++ src/pages/Home/components/AboutUs/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function AboutUs(props) {
+  return (
+    <div className="w-full h-[50vh] px-80 pt-10 flex flex-col items-center">
+      <div className="text-4xl font-light uppercase">
+        <span className="mb-2 border-b-2 border-red-500">about</span> us
+      </div>
+      <div className="text-justify pt-4">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit qui
+        quidem consequuntur quos assumenda voluptatum voluptate harum!
+        Doloremque consequatur nulla, sunt ea repellat odio error dicta. Iste
+        illum sint dolorum. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Eos sed, modi error, provident iste nulla sapiente earum, a natus
+        accusantium est corporis explicabo voluptatibus reiciendis. Possimus
+        omnis similique itaque obcaecati?Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Laudantium ratione ducimus nulla. Aspernatur quibusdam
+        maxime est! Laboriosam accusantium dolores ab natus ullam sint modi
+        provident debitis sit assumenda? Exercitationem, modi!Lorem Lorem ipsum,
+        dolor sit amet consectetur adipisicing elit. Eaque, aperiam sunt odit
+        tenetur qui reiciendis harum id, obcaecati unde nostrum voluptatibus
+        soluta expedita animi nisi quasi velit, veniam blanditiis maxime.
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,18 +5,14 @@ import {
   isBrowser,
   isMobile,
 } from "react-device-detect";
+import AboutUs from "./components/AboutUs";
 import Banner from "./components/Banner";
 
 function Home(props) {
   return (
     <>
       <Banner />
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias
-        explicabo nisi tempora porro officia ipsam voluptate sapiente, ipsum
-        rem. Soluta vitae ratione officiis iusto beatae temporibus accusantium
-        veritatis architecto suscipit.
-      </div>
+      <AboutUs />
     </>
     // <div>
     //   <BrowserView>
